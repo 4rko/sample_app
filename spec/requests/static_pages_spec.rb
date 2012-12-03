c1696914a1bf3d4598614bfdfa67f27e118ff9c5
Artur <artkopacz@gmail.com>
2012-12-03 18:33:01 +0100

    Finish layout and routes / Konczy uklad i trasy

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,21 +4,21 @@ describe "StaticPages" do
 
 	describe "Home Page" do
 		it "should have content 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_content('Sample App')
 		end
 	end
 
 	describe "Help Page" do
 		it "should have content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_content('Help')
 		end
 	end
 
 	describe "About Page" do
 		it "should have content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_content('About Us')
 		end
 	end
